2c50164b9f09916e2c86ce267795186e746a952f
Killmonger3432 <65647358+Killmonger3432@users.noreply.github.com>
2020-05-21 16:36:24 +0530

    Update Proj.py

--- Proj.py
+++ Proj.py
@@ -14,4 +14,16 @@ o2x,o2y=1100,90
 o3x,o3y=1100,320
 o4x,o4y=1300,160
 o5x,o5y=1300,255    
+b=pygame.image.load("Courtn2.png").convert()
+p1=pygame.image.load("ply.png").convert()
+p2=pygame.image.load("ply2.png").convert()
+p3=pygame.image.load("ply3.png").convert()
+p4=pygame.image.load("ply4.png").convert()
+p5=pygame.image.load("ply5.png").convert()
+o1=pygame.image.load("opp.png").convert()
+o2=pygame.image.load("opp2.png").convert()
+o3=pygame.image.load("opp3.png").convert()
+o4=pygame.image.load("opp4.png").convert()
+o5=pygame.image.load("opp5.png").convert()
+
 
