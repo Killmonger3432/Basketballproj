a122add2062a1391b903a423c45270994c876fea
Killmonger3432 <65647358+Killmonger3432@users.noreply.github.com>
2020-05-21 16:37:33 +0530

    Update Proj.py

--- Proj.py
+++ Proj.py
@@ -1,19 +1,3 @@
-import pygame
-import math
-import random
-import mysql.connector
-from sys import exit
-s=pygame.display.set_mode([1450,800])
-px,py=35,200
-p2x,p2y=1050,45
-p3x,p3y=1050,360
-p4x,p4y=1250,120
-p5x,p5y=1250,290
-ox,oy=960,200
-o2x,o2y=1100,90
-o3x,o3y=1100,320
-o4x,o4y=1300,160
-o5x,o5y=1300,255    
 b=pygame.image.load("Courtn2.png").convert()
 p1=pygame.image.load("ply.png").convert()
 p2=pygame.image.load("ply2.png").convert()
