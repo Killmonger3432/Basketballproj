35350a610ff58fc125e08a39a07ab1d33c5533c3
ahishbhat <65328249+ahishbhat@users.noreply.github.com>
2020-06-17 19:52:43 +0530

    Update movement.py

--- movement.py
+++ movement.py
@@ -1,4 +1,5 @@
 import pygame
+#import time
 pygame.init()
 mainloop = True
 screen = pygame.display.set_mode((1800, 1000))
@@ -12,13 +13,18 @@ walkdown = [pygame.image.load('down1.png'), pygame.image.load('down2.png'), pyga
             pygame.image.load('down4.png')]
 win = pygame.display.set_mode((1500, 800))
 char = pygame.image.load('down1.png')
+char2 = pygame.image.load('down1.png')
 clock = pygame.time.Clock()
-x = 50
-y = 400
+x2 = 150
+y2 = 400
+x1 = 50
+y1 = 400
 width = 64
 height = 64
 vel = 10
 up = False
+player1 = False
+player2 = False
 down = False
 jumpCount = 10
 left = False
@@ -26,55 +32,13 @@ right = False
 walkCount = 0
 char = pygame.image.load('down1.png')
 bg = pygame.image.load('w.png')
-def redrawGameWindow():
-    global walkCount
-    win.blit(bg, (0, 0))
-    if walkCount +1 >= 4:
-        walkCount = 0
-    if left and not up and not down and not right:
-        walkCount = walkCount + 1
-        win.blit(walkleft[walkCount], (x, y))
-        print("left")
-    elif right and not up and not down and not left:
-        walkCount = walkCount + 1
-        win.blit(walkright[walkCount],(x,y))
-        print("right")
-    elif up and not left and not right and not down:
-        win.blit(walkup[walkCount],(x,y))
-        walkCount = walkCount + 1
-        print("up")
-    elif down and not left and not right and not up:
-        win.blit(walkdown[walkCount],(x,y))
-        walkCount = walkCount + 1
-        print("down")
-    elif right and down and not left and not up:
-        win.blit(walkright[walkCount], (x, y))
-        walkCount = walkCount +1
-        print("4")
-    elif left and down and not right and not up:
-        win.blit(walkleft[walkCount], (x, y))
-        walkCount = walkCount + 1
-        print("3")
-    elif right and up and not left and not down:
-        win.blit(walkright[walkCount], (x, y))
-        walkCount = walkCount + 1
-        print("1")
-    elif left and up and not right and not down:
-        win.blit(walkleft[walkCount], (x, y))
-        walkCount = walkCount + 1
-        print("2")
-    else: #not left and not right and not up and not down:
-        win.blit(char, (x, y))
+def movement(keys,x,y,vel):
 
-        walkCount = 0
+    global left
+    global right
+    global up
+    global down
 
-    pygame.display.update()
-while mainloop:
-    clock.tick(12)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            mainloop = False
-    keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT] and not keys[pygame.K_UP] and not keys[pygame.K_DOWN] and not keys[pygame.K_RIGHT] and x > vel:
         x -= vel
         left = True
@@ -134,7 +98,77 @@ while mainloop:
         up = False
         down = False
         walkCount = 0
-    if up or down or right or left :
-        print(up,down,left,right)
-    redrawGameWindow()
+    return(x,y)
+
+def redrawGameWindow(x,y):
+    global walkCount
+    #win.blit(bg, (0, 0))
+    if walkCount +1 >= 4:
+        walkCount = 0
+    if left and not up and not down and not right:
+        walkCount = walkCount + 1
+        win.blit(walkleft[walkCount], (x, y))
+
+    elif right and not up and not down and not left:
+        walkCount = walkCount + 1
+        win.blit(walkright[walkCount],(x,y))
+
+    elif up and not left and not right and not down:
+        win.blit(walkup[walkCount],(x,y))
+        walkCount = walkCount + 1
+
+    elif down and not left and not right and not up:
+        win.blit(walkdown[walkCount],(x,y))
+        walkCount = walkCount + 1
+
+    elif right and down and not left and not up:
+        win.blit(walkright[walkCount], (x, y))
+        walkCount = walkCount +1
+
+    elif left and down and not right and not up:
+        win.blit(walkleft[walkCount], (x, y))
+        walkCount = walkCount + 1
+
+    elif right and up and not left and not down:
+        win.blit(walkright[walkCount], (x, y))
+        walkCount = walkCount + 1
+
+    elif left and up and not right and not down:
+        win.blit(walkleft[walkCount], (x, y))
+        walkCount = walkCount + 1
+
+    else: #not left and not right and not up and not down:
+        win.blit(char, (x, y))
+
+        walkCount = 0
+
+    #pygame.display.update()
+while mainloop:
+    clock.tick(12)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            mainloop = False
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_1]:
+        player1=True
+        player2= False
+    if keys[pygame.K_2]:
+        player1= False
+        player2 = True
+    if player1 :
+        x1, y1 = movement(keys, x1, y1, vel)
+        redrawGameWindow(x1,y1)
+        win.blit(char2, (x2, y2))
+    if player2 :
+        x2, y2 = movement(keys, x2, y2, vel)
+        redrawGameWindow(x2,y2)
+        win.blit(char, (x1, y1))
+    pygame.display.update()
+
+    win.blit(bg, (0, 0))
+    #if all([b == 0 for b in keys]):
+    #time.sleep(0.2)
+   # win.blit(char, (x1, y1))
+    #win.blit(char2, (x2, y2))
 pygame.quit()
