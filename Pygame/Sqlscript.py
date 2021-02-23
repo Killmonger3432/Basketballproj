a1dc84a8cd88b41afe5c9829f364db1cb3c581c2
Killmonger3432 <agasthyavalluru0112@gmail.com>
2021-02-23 09:30:29 +0530

    Fixed File Paths

--- Pygame/Sqlscript.py
+++ Pygame/Sqlscript.py
@@ -20,13 +20,12 @@ def exec_sql_file(cursor, sql_file):
     statement = ""
 
     for line in open(sql_file):
-        if re.match(r'--', line):  # ignore sql comment lines
+        if re.match(r'--', line):  
             continue
-        if not re.search(r';$', line):  # keep appending lines that don't end in ';'
+        if not re.search(r';$', line):  
             statement = statement + line
-        else:  # when you get a line ending in ';' then exec statement and reset for next statement
+        else:  
             statement = statement + line
-            #print "\n\n[DEBUG] Executing SQL statement:\n%s" % (statement)
             cursor.execute(statement)
 
             statement = ""
