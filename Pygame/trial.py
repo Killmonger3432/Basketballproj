11aea905084c4b1e9ff2af504e9e5b8e21c7a211
Killmonger3432 <65647358+Killmonger3432@users.noreply.github.com>
2021-02-23 09:24:46 +0530

    SS

--- Pygame/trial.py
+++ Pygame/trial.py
@@ -1,9 +1,10 @@
 import pathlib
-a=pathlib.Path(__file__).absolute()
-print(a)
-f=open(a)
 import os
 b=os.path.dirname(os.path.abspath("project_myteam.sql"))
 print("S",b)
 print("N",os.path.abspath(os.getcwd()))
-print(os.path.abspath("mysql_tables/project_myteam.sql"))
+k=os.path.abspath("mysql_tables/project_myteam.sql")
+f=open(k,"r")
+print(f.read(
+
+))
